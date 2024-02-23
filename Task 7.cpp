ac604910c14302362c5026c41a4c8460a1276e8e
Diana0302 <138877250+Diana0302@users.noreply.github.com>
2024-02-23 19:30:25 +0400

    Add files via upload

---
+++ Task 7.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+void BubbleSorting(int* arr, int size) {
+	for (int i = 0; i < size - 1; ++i) {
+		for (int j = 0; j < size - 1 - i; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				std::swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
+void SelectionSorting(int* arr, int size) {
+	int min;
+	for (int i = 0; i < size - 1; ++i) {
+		min = i;
+		for (int j = i + 1; j < size; ++j) {
+			if (arr[j] > arr[min]) {
+				min = j;
+			}
+			std::swap(arr[j], arr[min]);
+		}
+	}
+}
+
+
+int main() 
+{
+	int arr[20];
+	int size;
+	int a;
+	std::cout << "Enter the size of array: ";
+	std::cin >> size;
+	std::cout << "Enter the array: ";
+	for (int i = 0; i < size; ++i)
+		std::cin >> arr[i];
+	std::cout << "Choose 1 if you want to see Bubble sorting algorithm or choose 2 for Selestion: ";
+	std::cin >> a;
+	if (a == 1) {
+		BubbleSorting(arr, size);
+	}
+	else {
+		SelectionSorting(arr, size);
+	}
+std::cout << "ARRAY" << std::endl;
+	for (int i = 0; i < size; ++i) {
+		
+		std::cout << arr[i] << " ";
+		
+	}
+	std::cout << std::endl;
+	return 0;
+}
